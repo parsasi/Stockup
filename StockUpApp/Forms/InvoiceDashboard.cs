bbafb0cf0215c4e4883e367328cefc4ebae6095f
msafavi1@my.bcit.ca <msafavi1@my.bcit.ca>
2020-11-16 13:23:36 +0330

    Solution restructured StockUpApp project created to seperate the App from the data layer

---
+++ StockUpApp/Forms/InvoiceDashboard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockUp.Forms
+{
+    public partial class InvoiceDashboardForm : StockUpForm
+    {
+        public InvoiceDashboardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteInvoiceButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveInvoiceButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dashboardInvoiceButton_Click(object sender, EventArgs e)
+        {
+            FormNavigator.SwitchForm("DashboardForm");
+        }
+    }
+}
---
+++ StockUpApp/Forms/ProductDashboard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockUp.Forms
+{
+    public partial class ProductDashboardForm : StockUpForm
+    {
+        public ProductDashboardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dashboardButton_Click(object sender, EventArgs e)
+        {
+            FormNavigator.SwitchForm("DashboardForm");
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void addFormButton_Click(object sender, EventArgs e)
+        {
+            FormNavigator.OpenForm("addProductForm");
+        }
+    }
+}
