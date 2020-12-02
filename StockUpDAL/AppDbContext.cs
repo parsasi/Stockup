2faa5d79c2f0a26601a42e263f1b0b410a228134
Chris <cng177@my.bcit.ca>
2020-12-01 20:31:27 -0800

    Test env

--- StockUpDAL/AppDbContext.cs
+++ StockUpDAL/AppDbContext.cs
@@ -1,23 +1,27 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore;
 using StockUpBL.Models;
 
 
-namespace StockUpDAL 
+namespace StockUpDAL
 {
     public class AppDbContext : DbContext
     {
- 
+
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<Invoice> Invoices { get; set; } 
+        public DbSet<Invoice> Invoices { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\parsa\OneDrive\Documents\StockUpDB.mdf;Integrated Security=True;Connect Timeout=30";
-            optionsBuilder.UseSqlServer(connectionString);
+            DotNetEnv.Env.Load();
+            string connectionStringEnv = DotNetEnv.Env.GetString("CONNECTION_STRING");
+            Console.WriteLine(connectionStringEnv);
+
+            // var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\parsa\OneDrive\Documents\StockUpDB.mdf;Integrated Security=True;Connect Timeout=30";
+            // optionsBuilder.UseSqlServer(connectionString);
         }
 
     }
